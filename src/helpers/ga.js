5a68e0db5e7bb79afa76f1f84322c43ac674e515
Mark <mark@remarkablemark.org>
2018-03-03 22:58:03 -0500

    feat(helpers): create google analytics helper

---
+++ src/helpers/ga.js
@@ -0,0 +1,34 @@
+const { ga, requirejs } = window;
+
+ga('create', 'UA-59233605-5', 'auto');
+
+// https://developers.google.com/analytics/devguides/collection/analyticsjs/debugging
+let url = 'https://www.google-analytics.com/analytics.js';
+if (
+  process.env.NODE_ENV === 'development' ||
+  location.hostname === 'localhost'
+) {
+  url = url.replace('analytics.js', 'analytics_debug.js');
+  ga('set', 'sendHitTask', null);
+}
+
+ga('set', {
+  title: 'Sudoku',
+  page: '/sudoku/',
+});
+ga('send', 'pageview');
+
+requirejs([url]);
+
+/**
+ * Tracks event with analytics.
+ * https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+ *
+ * @param {String} eventCategory
+ * @param {String} eventAction
+ * @param {String} [eventLabel]
+ * @param {Number} [eventValue]
+ */
+export const trackEvent = (...args) => {
+  ga.apply(null, ['send', 'event', ...args]);
+};
