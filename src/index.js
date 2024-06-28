0faa11df02e5c982d7657272078665a277ecbf0c
Mark <remarkablemark@users.noreply.github.com>
2024-06-28 00:46:11 -0400

    fix: remove polyfill.io
    https://blog.cloudflare.com/automatically-replacing-polyfill-io-links-with-cloudflares-mirror-for-a-safer-internet/

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === 'development') {
   bootstrap();
 } else {
   window.requirejs(
-    ['https://cdn.polyfill.io/v2/polyfill.min.js?features=es5&flags=gated'],
     bootstrap,
     bootstrap // http://requirejs.org/docs/api.html#errbacks
   );
